3142e26ad9215e16531276929efe6c8df1c45386
Ahmed <94145850+iamklevy@users.noreply.github.com>
2023-12-14 20:00:37 +0200

    Add files via upload

--- Weather_Station.ino
+++ Weather_Station.ino
@@ -1,113 +1,230 @@
-#include <DHT.h>
-#include <SD.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085.h>
 #include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "ESP32_MailClient.h"
+#include <DHT.h>
 
-//Network credential 
+// Replace with your network credentials
 const char* ssid = "Fifi";
 const char* password = "o12w11h1a8s2";
 
-#define DHTPIN 15  // Digital pin connected to the DHT sensor
-#define CS_PIN 2
-#define DHTTYPE DHT22 
-#define ALTITUDE 200.0 //altitude in meters
-
+// DHT sensor
+#define DHTPIN 15     // Digital pin connected to the DHT sensor
+#define DHTTYPE    DHT22     // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
+// BMP180 sensor
 Adafruit_BMP085 bmp;
 
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-File dataFile;
-           
 String readDHTTemperature() {
-
-  float temperature = dht.readTemperature();
-
-  if (isnan(temperature)) {    
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
   else {
-    Serial.println(temperature);
-    return String(temperature);
+    Serial.println(t);
+    return String(t);
   }
 }
 
-String readDHTHumidity(){
-
-  float humidity = dht.readHumidity();
-
-  if(isnan(humidity)){
+String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
-   else {
-    Serial.println(humidity);
-    return String(humidity);
+  else {
+    Serial.println(h);
+    return String(h);
   }
 }
 
-void setup() {
+String readBMPTemperature() {
+  // Read temperature from BMP180 sensor
+  return String(bmp.readTemperature());
+}
 
+String readPressure() {
+  // Read pressure from BMP180 sensor
+  return String(bmp.readPressure());
+}
+
+String readAltitude() {
+  // Read altitude from BMP180 sensor
+  return String(bmp.readAltitude());
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+    .bmp-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 Weather Station</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">%</sup>
+  </p>
+  <p>
+    <i class="fas fa-bars" style="color:#38059e;"></i> 
+    <span class="bmp-labels">Altitude</span> 
+    <span id="altitude">%ALTITUDE%</span>
+    <sup class="units">meters</sup>
+  </p>
+  <p>
+    <i class="fas fa-bars" style="color:#00d6c8;"></i> 
+    <span class="bmp-labels">Pressure</span>
+    <span id="pressure">%PRESSURE%</span>
+    <sup class="units">hPa</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("altitude").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/altitude", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("pressure").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET
+
+", "/pressure", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with sensor values
+String processor(const String& var){
+  if(var == "TEMPERATURE"){
+    return readDHTTemperature();
+  }
+  else if(var == "HUMIDITY"){
+    return readDHTHumidity();
+  }
+  else if(var == "ALTITUDE"){
+    return readAltitude();
+  }
+  else if(var == "PRESSURE"){
+    return readPressure();
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
   Serial.begin(115200);
-  dht.begin();
-  SD.begin(CS_PIN);
-  
+  //Wire.begin (21, 22);
+
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
-     attempts++;
   }
-  Serial.println("IP address of ESP32 is : ");
+
+  // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
-  Serial.println(readDHTTemperature());
-  Serial.println(readDHTHumidity());
 
-  if (!bmp.begin()) 
-  {
-   Serial.println("Failed to initialize BMP180/BMP085 sensor. Check connections.");
-   while(1);
-  }
-   if (!SD.begin())
-  {
-    Serial.println("Failed to initialize SD card. Check connections.");
-    while (1);
+  dht.begin();
+
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP180 sensor, check wiring!");
   }
 
-  dataFile = SD.open("sensor_data.txt", FILE_WRITE);
-  dataFile.println("Temperature (DHT, BMP) [°C]\tHumidity [RH%]\tPressure [Pa]\tAltitude [m]");
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+  server.on("/altitude", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readAltitude().c_str());
+  });
+  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readPressure().c_str());
+  });
 
+  // Start server
+  server.begin();
 }
-void loop() {
 
-  float dhtTemperature = dht.readTemperature();
-  float dhtHumidity = dht.readHumidity();
-
-  float bmpTemperature = bmp.readTemperature();
-  float bmpPressure = bmp.readPressure();
-  float bmpAltitude = bmp.readAltitude();
-
-  Serial.println("Temperature from DHT sensor: " + String(dhtTemperature) + " °C");
-  Serial.println("Humidity from DHT sensor: " + String(dhtHumidity) + " %");
-  Serial.println("Temperature from BMP180/BMP085 sensor: " + String(bmpTemperature) + " °C");
-  Serial.println("Pressure from BMP180/BMP085 sensor: " + String(bmpPressure) + " Pa");
-  Serial.println("Altitude from BMP180/BMP085 sensor: " + String(bmpAltitude) + " meters");
-  Serial.println();
-
-  dataFile.print(dhtTemperature);
-  dataFile.print("\t");
-  dataFile.print(dhtHumidity);
-  dataFile.print("\t");
-  dataFile.print(bmpTemperature);
-  dataFile.print("\t");
-  dataFile.print(bmpPressure);
-  dataFile.print("\t");
-  dataFile.println(bmpAltitude);
-
-  delay(500);
-
+void loop(){
+  
 }
